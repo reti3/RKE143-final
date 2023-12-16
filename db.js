7481dd50fdd82e6df5e0707f10ed716a07c628f4
reti3 <98617068+reti3@users.noreply.github.com>
2023-12-16 22:42:21 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
+const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
-const { Pool } = require('pg');
-
 
 const itemsPool = new Pool({
     connectionString: process.env.DBConnectionString,
     ssl:{
         rejectUnauthorized: false
-    }
-    
+    }    
 });
 
 module.exports = itemsPool;
